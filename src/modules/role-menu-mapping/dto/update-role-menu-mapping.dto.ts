c3136243cf3af7e1ff8269dd255e0e6c3e4a3acf
AuroScholar <auroscholar@gmail.com>
2026-06-25 08:42:37 +0530

    Initial commit

--- src/modules/role-menu-mapping/dto/update-role-menu-mapping.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRoleMenuMappingDto } from './create-role-menu-mapping.dto';
-
-export class UpdateRoleMenuMappingDto extends PartialType(CreateRoleMenuMappingDto) {}
